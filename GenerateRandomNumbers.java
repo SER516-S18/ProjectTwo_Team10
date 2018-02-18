8f3b281815032d6f61b2a0987e7a963d0e32fb47
pvenugo <32775579+pvenugo@users.noreply.github.com>
2018-02-18 13:02:13 -0700

    Random Number Generation
    Initialise class with highest number possible , lowest number possible and number of channels , Keep calling function based on frequency :
    Ouput will be an array list with random numbers

---
+++ GenerateRandomNumbers.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GenerateRandomNumbers {
+	/**
+	 * @SER516 ProjecTwo_Team10
+	 * @author Prasanth Venugopal
+	 * @Version 1.0
+	 * User Story #3: Generation of Random values 
+	 */
+	int highestNumberPossible;
+	int lowestNumberPossible;
+	int Channel;
+	Random random = new Random();
+	public GenerateRandomNumbers(int high, int low,  int channel)
+	{	
+		 this.highestNumberPossible = high;
+		 this.lowestNumberPossible = low;
+		 this.Channel = channel;
+	}
+	public  void RandomNumberFunction() {
+		  
+		    	ArrayList<Integer> al = new ArrayList<Integer>();
+		    
+        		
+        		for(int i = 0; i < Channel;i++)
+        		{
+        		int randomNumber = random.nextInt(highestNumberPossible - lowestNumberPossible) + lowestNumberPossible;
+        		al.add(randomNumber);
+        		}
+        		System.out.print(al);   
+			}
+
+}
