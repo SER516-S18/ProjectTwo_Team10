df87c36c81c54973251716940706c009b7fa7dea
Parsons-Ray <32499337+Parsons-Ray@users.noreply.github.com>
2018-02-21 16:32:19 -0700

    Update startStopButton.java
    Adapt Socket

--- startStopButton.java
+++ startStopButton.java
@@ -1,32 +1,32 @@
-import java.awt.event.*;
-import javax.swing.*;
-import java.awt.*;
+import java.io.IOException;
+import java.net.Socket;
 /**
  * @SER516 ProjecTwo_Team10
  * @author Ruihao Zhou
- * @Version 1.0
+ * @Version 2.0
  * User Story #4: StartStopButton(Server)
  */
 
 
 public class startStopButton {
-    public static void main(String[] args){
-        JFrame frame = new JFrame("Client");
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
-        JPanel panel = new JPanel();
-        frame.add(panel);
-        JButton button = new JButton("Start/Stop");
-        panel.add(button)
-    }
-
-    static class Action implements ActionListener{
-        public void actionStarted(ActionEvent e){
-            JFrame frame2 = new JFrame("Clicked");
-            frame2.setVisable(true);
-            frame2.setSize(200,200);
-            JLabel label = new JLabel("Clicked done")
+    
+    Socket serverSocket = null;
+    
+    public static void start() {
+        try {
+            serverSocket = new Socket("localhost", 1516);
+            System.out.println("Connected succeffsully");
+        } catch (Exception e) {
+            e.printStackTrace("Start Now");
         }
     }
+    
+    public static void stop() {
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace("Stop Now");
+        }
+    }
+   
 }
