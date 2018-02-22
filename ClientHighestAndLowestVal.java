c0db21ae3d8a3793dbdfa0379d95366955f6b961
Terry <Weizitong@users.noreply.github.com>
2018-02-21 17:18:07 -0700

    Update ClientHighestAndLowestVal class

--- ClientHighestAndLowestVal.java
+++ ClientHighestAndLowestVal.java
@@ -1,45 +1,38 @@
+import java.util.List;
+
 /**
  * Class about highest and lowest value on the client side.
  */
 
-public class ClientHightestAndLowestVal {
-    private int highestVal;
-    private int lowestVal;
+public class ClientHighestAndLowestVal {
+    private static int highestVal;
+    private static int lowestVal;
 
-    public ClientHightestAndLowestVal() {
+    static {
         highestVal = 0;
         lowestVal = 0;
     }
 
-    public ClientHightestAndLowestVal(int h, int l) {
-        highestVal = h;
-        lowestVal = l;
-    }
+    private ClientHighestAndLowestVal() {}
 
-    /**
-     * @param highestVal the highestVal to set
-     */
-    public void setHighestVal(int highestVal) {
-        this.highestVal = highestVal;
+    public static void readList(List<Integer> list) {
+        for (int i : list) {
+            highestVal = i > highestVal ? i : highestVal;
+            lowestVal = i < lowestVal ? i : lowestVal;
+        }
     }
 
     /**
      * @return the highestVal
      */
-    public int getHighestVal() {
+    public static int getHighestVal() {
         return highestVal;
     }
 
     /**
-     * @param lowestVal the lowestVal to set
-     */
-    public void setLowestVal(int lowestVal) {
-        this.lowestVal = lowestVal;
-    }
-    /**
      * @return the lowestVal
      */
-    public int getLowestVal() {
+    public static int getLowestVal() {
         return lowestVal;
     }
 }
