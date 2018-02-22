f9c051f4a596b9455b3945e622be4e1a8e33d80c
Raghav1794 <35378167+Raghav1794@users.noreply.github.com>
2018-02-22 12:05:13 -0700

    Add files via upload

---
+++ ClientFrequency.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+/**
+ * Reads frequency from the GUI
+ * @author RaghavendraPrasadVemula
+ * @version 1.0
+ */
+
+public class ReadFrequency{
+
+    final int defaultFrequency = 1;
+
+    /**
+     * Takes input field for frequency and returns it if valid, or returns default value
+     * @param frequency
+     */
+
+    public int getFrequency(JTextPane frequency){
+
+        int frequencyValue = defaultFrequency;
+        if(frequency.getText() != ""){
+            try{
+                frequencyValue = Integer.parseInt(frequency.getText());
+            } catch(Exception e){
+                System.out.println("Non-integer value entered for frequency, default used.");
+            }
+        }
+
+        return frequencyValue;
+    }
+
+}
