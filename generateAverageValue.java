aca4d9ac6bbfc34e8aa7e93cf7ef779cd960a78d
eastalent <35270482+eastalent@users.noreply.github.com>
2018-02-21 18:05:48 -0700

    Update generateAverageValue.java

--- generateAverageValue.java
+++ generateAverageValue.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
  * @Version 1.0
  * User Story #12: average value in the client side
  */
+ArrayList<Integer> valueList = new ArrayList<Integer>();
 public class generateAverageValue {
     /**
      * @param valueList  a list of values get from server
      */
-    public int getAverage(ArrayList<Integer> valueList){
+    public int getAverage(){
         long sum = 0;
         for(int v : valueList){
             sum += v;
