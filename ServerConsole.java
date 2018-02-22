414c8b035f3518bea30c8aac1a58600cd24c2a99
harisiddarthvk <35482014+harisiddarthvk@users.noreply.github.com>
2018-02-22 11:20:56 -0700

    Update ServerConsole.java

--- ServerConsole.java
+++ ServerConsole.java
@@ -1,31 +1,27 @@
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.swing.JTextPane;
+import javax.swing.JEditorPane;
 import javax.swing.text.html.HTMLDocument;
 
 public class ServerConsole {
-	static JTextPane console = ServerGUI.getTextPane();
-	private final static Logger LOGGER = Logger.getLogger(ServerGUI.class.getName());
+	static JEditorPane console = ServerGUI.getTextPane();
 
 	public static void setErrorMessage(String error) {
 		try {
 			console.setContentType( "text/html" );
-			HTMLDocument doc=(HTMLDocument) console.getStyledDocument();
-            		doc.insertAfterEnd(doc.getCharacterElement(doc.getLength()),"<span style=\"color:red\">" + new Date() + " " + error + "</span> <br>");
-		} catch (Exception ex) {
-			LOGGER.log(Level.SEVERE, "Error adding message to console", ex);
+			HTMLDocument doc=(HTMLDocument) console.getDocument();
+            doc.insertAfterEnd(doc.getCharacterElement(doc.getLength()),"<span style=\"color:red\">" + "[ " + new Date() + " ] " + error + "</span> <br>");
+		} catch (Exception e) {
+			console.setText("Error printing to Console");
 		}
 	}
 	
 	public static void setMessage(String message) {
 		try {
 			console.setContentType( "text/html" );
-			HTMLDocument doc=(HTMLDocument) console.getStyledDocument();
-			doc.insertAfterEnd(doc.getCharacterElement(doc.getLength()),"<span style=\"color:black\">" + new Date() + " " + message + "</span> <br>");
-		} catch (Exception ex) {
-			LOGGER.log(Level.SEVERE, "Error adding message to console", ex);
+			HTMLDocument doc=(HTMLDocument) console.getDocument();
+			doc.insertAfterEnd(doc.getCharacterElement(doc.getLength()),"<span style=\"color:black\">" + "[ " + new Date() + " ] " + message + "</span> <br>");
+		} catch (Exception e) {
+			console.setText("Error printing to Console");
 		}
 	}
 }
