3806054199ceb6de3e29648ca1c1b56c05b1174f
pvenugo <32775579+pvenugo@users.noreply.github.com>
2018-02-22 19:37:58 -0700

    Bug FIx
    Bug fix

--- ClientWindow.java
+++ ClientWindow.java
@@ -1,18 +1,14 @@
+
 import java.awt.EventQueue;
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
@@ -20,7 +16,6 @@ import java.awt.Color;
 import java.awt.SystemColor;
 import java.awt.Panel;
 import java.awt.Font;
-import java.awt.List;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextPane;
@@ -31,10 +26,7 @@ public class ClientWindow extends JFrame implements ActionListener {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	//private JPanel contentPane;
 	private JFrame frame;
-	private static int flag = 0;
-	static JTextPane txtpnConsole = new JTextPane();
 
 	/**
 	 * Launch the application.
@@ -43,8 +35,8 @@ public class ClientWindow extends JFrame implements ActionListener {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ClientWindow window = new ClientWindow();
-					window.frame.setVisible(true);
+					ClientWindow clientwindow = new ClientWindow();
+					clientwindow.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -56,160 +48,148 @@ public class ClientWindow extends JFrame implements ActionListener {
 	 * Create the frame.
 	 */
 	public ClientWindow() {
-		/*setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 550);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);*/
-		
 		frame = new JFrame();
 		frame.setTitle("Client");
 		frame.setBounds(100, 100, 800, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JButton btnStartstop = new JButton("Start/Stop");
 		btnStartstop.setBackground(new Color(255, 182, 193));
 		btnStartstop.setFont(new Font("Courier New", Font.PLAIN, 16));
 		btnStartstop.setBounds(604, 16, 159, 29);
 		frame.getContentPane().add(btnStartstop);
-		if(btnStartstop.isEnabled())
-			flag = 1;
-		else 
-			flag = 0;
-		
+
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(220, 220, 220));
+		panel.setBackground(SystemColor.controlHighlight);
 		panel.setBounds(15, 58, 748, 300);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(173, 216, 230));
+		panel_1.setBackground(SystemColor.activeCaption);
 		panel_1.setBounds(433, 16, 157, 38);
 		panel.add(panel_1);
-		
+
 		JLabel lblHighestValue = new JLabel("Highest Value:");
 		lblHighestValue.setFont(new Font("Courier New", Font.PLAIN, 16));
 		panel_1.add(lblHighestValue);
-		
+
 		JPanel panel_2 = new JPanel();
 		panel_2.setBackground(new Color(255, 182, 193));
 		panel_2.setBounds(433, 70, 157, 38);
 		panel.add(panel_2);
-		
+
 		JLabel lblLowestValue = new JLabel("Lowest Value:");
 		lblLowestValue.setFont(new Font("Courier New", Font.PLAIN, 16));
 		panel_2.add(lblLowestValue);
-		
+
 		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(173, 216, 230));
+		panel_3.setBackground(SystemColor.activeCaption);
 		panel_3.setBounds(433, 124, 157, 38);
 		panel.add(panel_3);
-		
+
 		JLabel lblNewLabel = new JLabel("Average:");
 		lblNewLabel.setFont(new Font("Courier New", Font.PLAIN, 16));
 		panel_3.add(lblNewLabel);
-		
+
 		JPanel panel_4 = new JPanel();
 		panel_4.setBackground(new Color(255, 182, 193));
 		panel_4.setBounds(433, 178, 157, 38);
 		panel.add(panel_4);
-		
+
 		JLabel lblChannels = new JLabel("Channels:");
 		lblChannels.setFont(new Font("Courier New", Font.PLAIN, 16));
 		panel_4.add(lblChannels);
-		
+
 		JPanel panel_5 = new JPanel();
-		panel_5.setBackground(new Color(173, 216, 230));
+		panel_5.setBackground(SystemColor.activeCaption);
 		panel_5.setBounds(433, 232, 157, 41);
 		panel.add(panel_5);
-		
+
 		JLabel lblFrequencyhz = new JLabel("Frequency(Hz):");
 		lblFrequencyhz.setFont(new Font("Courier New", Font.PLAIN, 16));
 		panel_5.add(lblFrequencyhz);
-		
+
 		JPanel panel_6 = new JPanel();
 		panel_6.setBackground(new Color(255, 182, 193));
 		panel_6.setBounds(618, 16, 103, 38);
 		panel.add(panel_6);
-		
+
 		JLabel lblA = new JLabel("");
 		lblA.setFont(new Font("Courier New", Font.PLAIN, 16));
 		panel_6.add(lblA);
-		
+
 		JPanel panel_7 = new JPanel();
-		panel_7.setBackground(new Color(173, 216, 230));
+		panel_7.setBackground(SystemColor.activeCaption);
 		panel_7.setBounds(618, 70, 103, 38);
 		panel.add(panel_7);
-		
+
 		JLabel label_1 = new JLabel("");
 		label_1.setFont(new Font("Courier New", Font.PLAIN, 16));
 		panel_7.add(label_1);
-		
+
 		JPanel panel_8 = new JPanel();
 		panel_8.setBackground(new Color(255, 182, 193));
 		panel_8.setBounds(618, 124, 103, 38);
 		panel.add(panel_8);
-		
+
 		JLabel label_2 = new JLabel("");
 		label_2.setFont(new Font("Courier New", Font.PLAIN, 16));
 		panel_8.add(label_2);
-	
-        String[] noChannels = {"1", "2", "3", "4", "5"};    
+
+		String[] noChannels = {"1", "2", "3", "4", "5"};    
 		JComboBox comboBox = new JComboBox(noChannels);
-        setNoOfChannels(1);
+		setNoOfChannels(1);
 		comboBox.addActionListener(this);
 		comboBox.setFont(new Font("Courier New", Font.PLAIN, 16));
-		comboBox.setBackground(new Color(173, 216, 230));
+		comboBox.setBackground(SystemColor.activeCaption);
 		comboBox.setBounds(618, 190, 103, 26);
 		panel.add(comboBox);
-		
+
 		JPanel panel_9 = new JPanel();
 		panel_9.setBackground(new Color(255, 182, 193));
 		panel_9.setBounds(618, 232, 103, 41);
 		panel.add(panel_9);
-		
+
 		JLabel label_3 = new JLabel("");
 		label_3.setFont(new Font("Courier New", Font.PLAIN, 16));
 		panel_9.add(label_3);
-		
+
 		Panel panel_10 = new Panel();
 		panel_10.setBackground(new Color(255, 182, 193));
 		panel_10.setBounds(10, 16, 406, 257);
 		panel.add(panel_10);
-		
-		
-		txtpnConsole.setFont(new Font("Courier New", Font.PLAIN, 16));
-		txtpnConsole.setBackground(new Color(173, 216, 230));
+
+		JTextPane txtpnConsole = new JTextPane();
+		//txtpnConsole.setFont(new Font("Courier New", Font.PLAIN, 16));
+		txtpnConsole.setBackground(SystemColor.controlHighlight);
 		txtpnConsole.setText("Console:");
 		txtpnConsole.setBounds(15, 388, 748, 90);
+		txtpnConsole.setEditable(false);
 		frame.getContentPane().add(txtpnConsole);
 
-        startClient();
+		startClient();
 	}
-	
-     public static JTextPane getTextPane() {
-			return txtpnConsole;
-     }
-	
+
 	/**
 	 * Action listner for drop down menu
 	 */
 	public void actionPerformed(ActionEvent e) {
-        JComboBox cb = (JComboBox)e.getSource();
-        Integer no = Integer.parseInt((String)cb.getSelectedItem());
-        setNoOfChannels(no);
-    	}
-	
-	public static void shrinkTo(ArrayList<Integer> list, int channel) {
-	int size = list.size();
-	if (channel >= size) return;
-	for (int i = channel; i < size; i++) {
-	list.remove(list.size() - 1);
+		JComboBox cb = (JComboBox)e.getSource();
+		Integer no = Integer.parseInt((String)cb.getSelectedItem());
+		setNoOfChannels(no);
 	}
-	
+
+	public static void shrinkTo(ArrayList<Integer> list, int channel) {
+		int size = list.size();
+		if (channel >= size) return;
+		for (int i = channel; i < size; i++) {
+			list.remove(list.size() - 1);
+		}
+	}
 
 
 	public static void startClient() {
@@ -249,13 +229,13 @@ public class ClientWindow extends JFrame implements ActionListener {
 		}).start();
 	}
 
-    synchronized static Integer getNoOfChannels() {
-        return noOfChannels;
-    }
+	synchronized static Integer getNoOfChannels() {
+		return noOfChannels;
+	}
 
-    synchronized static void setNoOfChannels(Integer x) {
-        noOfChannels = x;
-    }
- 
-    private static Integer noOfChannels;
+	synchronized static void setNoOfChannels(Integer x) {
+		noOfChannels = x;
+	}
+
+	private static Integer noOfChannels;
 }
