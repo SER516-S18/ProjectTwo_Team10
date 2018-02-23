71afa0ea552c888b734bbd01dad5f553b77e94c3
byan17 <35480884+byan17@users.noreply.github.com>
2018-02-23 16:10:09 -0700

    Update ClientConsole.java
    add comment, fix the structure, fix the bug

--- ClientConsole.java
+++ ClientConsole.java
@@ -3,34 +3,44 @@ import java.awt.*;
 import java.util.Date;
 import java.util.logging.*;
 import javax.swing.text.html.HTMLDocument;
+
 /**
-* @SER516-Project 2
-* @author  Binbin Yan
-* @version 0.9
-* @since   02/17/2018
-*/
+  * @SER516  Project two
+  * @author  Binbin Yan
+  * @version 1.0
+  * @since   02/21/2018
+  * User Story #13: Console on Client GUI.
+  */
+
 public class ClientConsole{
-    
     static JTextPane console =  ClientWindow.getTextPane();
     private final static Logger printing = Logger.getLogger(ClientWindow.class.getName());
     
+    /**
+      * Get srror string and display it on the console.
+      * @param error
+      */
     public static void setErrorMessage(String error) {
-	    try {
-		    console.setContentType( "text/html" );
-		    HTMLDocument disp1=(HTMLDocument) console.getStyledDocument();
-		    disp1.insertAfterEnd(disp1.getCharacterElement(disp1.getLength()),"<span style=\"color:red\">" + new Date() + " " + error + "</span> <br>");
-	    } catch (Exception ex) {
-		    printing.log(Level.SEVERE, "Error adding message to console", ex);
-	    }
+        try {
+                    console.setContentType( "text/html" );
+                    HTMLDocument disp1=(HTMLDocument) console.getStyledDocument();
+                    disp1.insertAfterEnd(disp1.getCharacterElement(disp1.getLength()),"<span style=\"color:red\">" + new Date() + " " + error + "</span> <br>");
+        } catch (Exception ex) {
+                    printing.log(Level.SEVERE, "Error adding message to console", ex);
+        }
     }
-	
+
+    /**
+      * Get message string and display it on the console.
+      * @param message
+      */
     public static void setMessage(String message) {
-	    try {
-		    console.setContentType( "text/html" );
-		    HTMLDocument disp2=(HTMLDocument) console.getStyledDocument();
-		    disp2.insertAfterEnd(disp2.getCharacterElement(disp2.getLength()),"<span style=\"color:black\">" + new Date() + " " + message + "</span> <br>");
-	    } catch (Exception ex) {
-		    printing.log(Level.SEVERE, "Error adding message to console", ex);
-	    }
+        try {
+                console.setContentType( "text/html" );
+                HTMLDocument disp2=(HTMLDocument) console.getStyledDocument();
+                disp2.insertAfterEnd(disp2.getCharacterElement(disp2.getLength()),"<span style=\"color:black\">" + new Date() + " " + message + "</span> <br>");
+        } catch (Exception ex) {
+                printing.log(Level.SEVERE, "Error adding message to console", ex);
+        }
     }
 }
