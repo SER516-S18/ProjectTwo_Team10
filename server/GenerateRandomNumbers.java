737ef10c5dd905dd486014107d377a3b03d9bf64
pvenugo <32775579+pvenugo@users.noreply.github.com>
2018-02-23 21:28:12 -0700

    Generate Random Number updated
    Comments , Formatting was done

--- server/GenerateRandomNumbers.java
+++ server/GenerateRandomNumbers.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
+ *The Generate Random Numbers class basically generates Random numbers between 
+ * the lowest value and the highest value provided
  * @SER516 ProjecTwo_Team10
  * @author Prasanth Venugopal
- * @Version 1.0
- * Generation of random values on server with given range
+ * @Version 1.0 
  */
 
 public class GenerateRandomNumbers {
@@ -16,21 +17,28 @@ public class GenerateRandomNumbers {
         lowestNumberPossible,
         channel;
 	
-	//Parameterized constructor
+	/* Initializes to store values to generate random values
+	 *@param high Random Number Higher range Limit
+	 *@param low Random Number Lower range limit
+	 *@param channel To generate channel number of Random Number
+	*/
 	public GenerateRandomNumbers(int high, int low,  int channel){	
 		this.highestNumberPossible = high;
 		this.lowestNumberPossible = low;
 		this.channel = channel;
 	}
 	
-	//Method to generate random values
+	/* Method to generate random values
+	 *@return arrayList
+	*/
 	public ArrayList<Integer> RandomNumberFunction() {
 		Random random = new Random();
-		ArrayList<Integer> al = new ArrayList<Integer>();
+		ArrayList<Integer> randomValueList = new ArrayList<Integer>();
         		for(int i = 0; i < channel;i++){
-        			int randomNumber = random.nextInt(highestNumberPossible - lowestNumberPossible) + lowestNumberPossible;
-        			al.add(randomNumber);
+        			int randomNumber = random.nextInt(highestNumberPossible - lowestNumberPossible) 
+					+ lowestNumberPossible;
+        			randomValueList.add(randomNumber);
         		}   
-        		return al;
+        		return randomValueList;
 	}
 }
