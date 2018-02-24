84a327192ad91987027078fb53f1518e94435116
Kanchan-Wakchaure <35479753+Kanchan-Wakchaure@users.noreply.github.com>
2018-02-23 22:47:56 -0700

    Update ServerConsole.java

--- server/ServerConsole.java
+++ server/ServerConsole.java
@@ -1,260 +1,42 @@
 package server;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
+import java.util.Date;
 import javax.swing.JEditorPane;
-
-import java.awt.Font;
-import java.awt.Color;
-import java.awt.Dimension;
-
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.JTextPane;
-import javax.swing.JTextField;
-import javax.swing.border.LineBorder;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.text.html.HTMLDocument;
 
 /**
  * @SER516 ProjecTwo_Team10
- * @author Kanchan Wakchaure 
- *         Vikram Wathodkar
- * @version 1.0 
- * Server GUI to start/stop the server and enter the value ranges
- */
+ * @author Hari Siddarth V Kesavan
+ * @version 1.0
+ * To print messages and errors on console in server GUI
+*/
 
-public class ServerGUI implements ActionListener {
+public class ServerConsole {
+	static JEditorPane console = ServerGUI.getTextPane();
 
-	// Server components
-	private JFrame frmServer;
-	private static ServerSocket serverSocket;
-	private static int serverState = 0;
-	static JTextPane txtHighValue = new JTextPane();
-	static JTextPane txtLowValue = new JTextPane();
-	static JTextPane consolePane = new JTextPane();
-	static JTextPane indicatorPane = new JTextPane();
-	static JTextField txtFrequency = new JTextField();
-	static Indicator ledIndicator = new Indicator(serverState);
-	static Socket socket;
-
-	// Server constructor
-	public ServerGUI() {
-
-		frmServer = new JFrame();
-		frmServer.setTitle("Server");
-		frmServer.setBounds(100, 100, 670, 588);
-		frmServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmServer.getContentPane().setLayout(null);
-
-		JPanel panel = new JPanel();
-		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel.setBackground(new Color(220, 220, 220));
-		panel.setBounds(12, 59, 628, 323);
-		frmServer.getContentPane().add(panel);
-		panel.setLayout(null);
-
-		//startStop button
-		JButton btnStartStop = new JButton("start / stop");
-		btnStartStop.setBackground(new Color(255, 204, 204));
-		btnStartStop.setFont(new Font("Courier New", Font.PLAIN, 18));
-		btnStartStop.setBounds(436, 13, 192, 34);
-		btnStartStop.setBorder(BorderFactory.createLineBorder(Color.black));
-		btnStartStop.addActionListener(this);
-
-		frmServer.getContentPane().add(btnStartStop);
-
-		//Highest value label
-		JLabel lblHighValue = new JLabel("<html>Highest<br>value:</html>");
-		lblHighValue.setBounds(345, 13, 133, 61);
-		panel.add(lblHighValue);
-		lblHighValue.setBackground(new Color(173, 216, 230));
-		lblHighValue.setOpaque(true);
-		lblHighValue.setHorizontalAlignment(SwingConstants.CENTER);
-		lblHighValue.setFont(new Font("Courier New", Font.PLAIN, 18));
-		lblHighValue.setBorder(BorderFactory.createLineBorder(Color.black));
-
-		//Lowest value label
-		JLabel lblLowValue = new JLabel("<html>Lowest<br>value:</html>");
-		lblLowValue.setBounds(345, 87, 133, 61);
-		panel.add(lblLowValue);
-		lblLowValue.setHorizontalAlignment(SwingConstants.CENTER);
-		lblLowValue.setFont(new Font("Courier New", Font.PLAIN, 18));
-		lblLowValue.setBorder(BorderFactory.createLineBorder(Color.black));
-		lblLowValue.setOpaque(true);
-		lblLowValue.setBackground(new Color(255, 204, 204));
-
-		//Frequency label
-		JLabel lblFrequency = new JLabel("<html>Frequency<br>(Hz):</html>");
-		lblFrequency.setBounds(345, 161, 133, 61);
-		panel.add(lblFrequency);
-		lblFrequency.setHorizontalAlignment(SwingConstants.CENTER);
-		lblFrequency.setFont(new Font("Courier New", Font.PLAIN, 18));
-		lblFrequency.setBorder(BorderFactory.createLineBorder(Color.black));
-		lblFrequency.setBackground(new Color(173, 216, 230));
-		lblFrequency.setOpaque(true);
-
-		//Highest value textField
-		txtHighValue.setBounds(490, 13, 126, 61);		
-		txtHighValue.setBackground(new Color(255, 204, 204));
-		txtHighValue.setFont(new Font("Courier New", Font.PLAIN, 18));
-		txtHighValue.setBorder(BorderFactory.createLineBorder(Color.black));
-		panel.add(txtHighValue);
-		
-		//Lowest value textField
-		txtLowValue.setBounds(490, 87, 126, 61);
-		panel.add(txtLowValue);
-		txtLowValue.setBackground(new Color(173, 216, 230));
-		txtLowValue.setFont(new Font("Courier New", Font.PLAIN, 18));
-		txtLowValue.setBorder(BorderFactory.createLineBorder(Color.black));
-
-		//Frequency textField
-		txtFrequency.setFont(new Font("Courier New", Font.PLAIN, 18));
-		txtFrequency.setBounds(490, 161, 126, 61);
-		panel.add(txtFrequency);
-		txtFrequency.setHorizontalAlignment(SwingConstants.CENTER);
-		txtFrequency.setBackground(new Color(255, 204, 204));
-		txtFrequency.setBorder(BorderFactory.createLineBorder(Color.black));
-
-		//Indicator panel for server status
-		indicatorPane.setBounds(22, 13, 311, 296);
-		panel.add(indicatorPane);
-		indicatorPane.setBackground(new Color(255, 204, 204));
-		indicatorPane.setFont(new Font("Courier New", Font.PLAIN, 18));
-		indicatorPane.setBorder(BorderFactory.createLineBorder(Color.black));
-		indicatorPane.setEditable(false);
-
-		//Calling the indicator
-		ledIndicator.setBorder(BorderFactory.createLineBorder(Color.black));
-		ledIndicator.setBounds(22, 13, 311, 296);
-		indicatorPane.add(ledIndicator);
-
+	public static void setErrorMessage(String error) {
 		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
+			console.setContentType( "text/html" );
+			HTMLDocument doc=(HTMLDocument) console.getDocument();
+            doc.insertAfterEnd(doc.getCharacterElement(doc.getLength()),
+               "[ " + new Date() +  " ] " + "<span style=\"color:red\">"  
+                + error + "</span> <br>");
+		} 
+		catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		JEditorPane consolePanel = new JEditorPane();
-		consolePane.setFont(new Font("Courier New", Font.PLAIN, 18));
-		consolePanel.setFont(new Font("Courier New", Font.PLAIN, 18));
-		consolePanel.setBackground(new Color(211, 211, 211));
-		consolePanel.setBounds(12, 390, 628, 138);
-		consolePanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		consolePanel.setLayout(new BoxLayout(consolePanel, BoxLayout.PAGE_AXIS));
-		consolePanel.setVisible(true);
-		
-		JTextPane lblConsole = new JTextPane();
-		lblConsole.setFont(new Font("Courier New", Font.PLAIN, 18));
-		lblConsole.setText(" Console: ");
-		lblConsole.setOpaque(false);
-		lblConsole.setBorder(BorderFactory.createLineBorder(new Color(211, 211, 211)));
-
-		JScrollPane consoleScroll = new JScrollPane(consolePane);
-		consoleScroll.setOpaque(false);
-		consoleScroll.setBorder(BorderFactory.createLineBorder(new Color(211, 211, 211)));
-		consoleScroll.setPreferredSize(new Dimension(600, 118));
-
-		consolePanel.add(lblConsole);
-		consolePanel.add(consoleScroll);
-
-		frmServer.getContentPane().add(consolePanel);
-
-		consolePane.setBorder(BorderFactory.createLineBorder(new Color(211, 211, 211)));
-		consolePane.setEditable(false);
-		consolePane.setBackground(new Color(211, 211, 211));
-		consolePane.setLayout(new BoxLayout(consolePane, BoxLayout.Y_AXIS));
-		consolePane.setEditable(false);
-		consolePane.setBackground(new Color(211, 211, 211));
 	}
-
-	public static JTextPane getTextPane() {
-		return consolePane;
-	}
-       
-    // Start and stop server actions  
-	public void actionPerformed(ActionEvent e) {
-		if (getServerState() == 0) {
-			setServerState(1);
-			ledIndicator.update(getServerState());
-		} else if (getServerState() == 1) {
-			setServerState(0);
-			ledIndicator.update(getServerState());
+	
+	public static void setMessage(String message) {
+		try {
+			console.setContentType( "text/html" );
+			HTMLDocument doc=(HTMLDocument) console.getDocument();
+			doc.insertAfterEnd(doc.getCharacterElement(doc.getLength()), 
+			  "[ " +  new Date() + " ] " + "<span style=\"color:black\">" 
+			   + message + "</span> <br>");
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-
-	//Getter method for serverState
-	synchronized public static int getServerState() {
-		return serverState;
-	}
-	
-	//Setter method for serverState
-	synchronized public static void setServerState(int state) {
-		serverState = state;
-	}
-	
-	// Main function to launch the application
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ServerGUI window = new ServerGUI();
-					window.frmServer.setVisible(true);
-					startServerThread();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	// Method to start the connection between server and client
-	public static void startServerThread() {
-		Thread connect = (new Thread() {
-            @Override
-            public void run() {
-        		int high, low, frequency;
-                try {
-                	serverSocket = new ServerSocket(9090);
-					socket = serverSocket.accept();
-					high = Integer.parseInt(ServerGUI.txtHighValue.getText());
-					low = Integer.parseInt(ServerGUI.txtLowValue.getText());
-					frequency = Integer.parseInt(ServerGUI.txtFrequency.getText());
-					GenerateRandomNumbers grn = new GenerateRandomNumbers(high, low,5);
-					while (true) {
-						while (getServerState() == 0)
-							Thread.sleep(1000 / frequency);
-						ArrayList<Integer> arrayList = grn.RandomNumberFunction();
-						System.out.print(arrayList);
-						ObjectOutputStream objectOutput = new 
-								ObjectOutputStream(socket.getOutputStream());
-						objectOutput.writeObject(arrayList);
-						System.out.println(arrayList);
-						Thread.sleep(1000 / frequency);
-						arrayList.clear();
-					}
-                } 
-                catch (Exception e) {
-                	e.printStackTrace();
-				}
-			}
-		});
-		connect.start();
-	}
-
 }
